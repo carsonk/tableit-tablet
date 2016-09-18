021f8236ec67cfa80b931fdafc9a2199c4c2bc1d
Carson Killbreath <kill4992@kettering.edu>
2016-09-18 01:10:08 -0400

    Parse menu from server! And better pagination...

--- tablet/tablet.ino
+++ tablet/tablet.ino
@@ -46,33 +46,35 @@ Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 
 #define ITEMS_PER_PAGE 4
 
-int currentcolor;
-
 struct MenuItem {
   int id;
-  char title[10];
+  char title[12];
   int price;
 };
 
 MenuItem menu_list[20];
-char num_items = 20;
+char num_items = 0;
 char current_page = 1;
 int total_cost = 0;
 
 int cart_menu_ids[20];
 int total_cart_items = 0;
 
+char connected_to_server = 0;
+
 #define PREVIOUS_BOX_X MARGIN
 #define PREVIOUS_BOX_Y MARGIN
+
+#define CONFIRM_BOX_X PREVIOUS_BOX_X + BOXSIZE + MARGIN
+#define CONFIRM_BOX_Y MARGIN
 
 #define NEXT_BOX_X (tft.width() - BOXSIZE - MARGIN)
 #define NEXT_BOX_Y MARGIN
 
 IPAddress ip(192, 168, 0, 117);
 EthernetClient client;
-StaticJsonBuffer<200> jsonBuffer;
 
-char server[] = "www.google.com";
+char server[] = "104.131.189.231";
 
 void draw_page() {
   int i = 0;
@@ -84,23 +86,33 @@ void draw_page() {
   tft.fillScreen(WHITE);
   tft.setTextSize(2);
   tft.setTextColor(WHITE);
-  
-  tft.fillRect(PREVIOUS_BOX_X, PREVIOUS_BOX_Y, BOXSIZE, BOXSIZE, RED); // Page back.
+
+  // Page back
+  tft.fillRect(PREVIOUS_BOX_X, PREVIOUS_BOX_Y, BOXSIZE, BOXSIZE, RED);
   tft.setCursor(PREVIOUS_BOX_X + MARGIN, PREVIOUS_BOX_Y + MARGIN);
   tft.print('<');
-  tft.fillRect(NEXT_BOX_X, NEXT_BOX_Y, BOXSIZE, BOXSIZE, RED); // Page forward.
+
+  // Confirm
+  tft.fillRect(CONFIRM_BOX_X, CONFIRM_BOX_Y, tft.width() - 2 * BOXSIZE - 4 * MARGIN, BOXSIZE, GREEN);
+  tft.setCursor(CONFIRM_BOX_X + MARGIN, CONFIRM_BOX_Y + MARGIN);
+  tft.print("Confirm");
+
+  // Page forward
+  tft.fillRect(NEXT_BOX_X, NEXT_BOX_Y, BOXSIZE, BOXSIZE, RED);
   tft.setCursor(NEXT_BOX_X + MARGIN, NEXT_BOX_Y + MARGIN);
   tft.print('>');
 
   for(i = 0; i < ITEMS_PER_PAGE; i++) {
     current_item = menu_list[i + (ITEMS_PER_PAGE * (current_page - 1))];
-    y_pos = i * (MENU_ITEM_CONTAINER_HEIGHT + MARGIN) + HEADER_SIZE;
-    tft.fillRect(MARGIN, y_pos, tft.width() - MARGIN * 2, MENU_ITEM_CONTAINER_HEIGHT, BLACK);
-    tft.setCursor(MARGIN * 2, y_pos + 10);
-    tft.print(current_item.title);
-    tft.setCursor(tft.width() - MARGIN - 40, y_pos + 10);
-    tft.print("$");
-    tft.print(current_item.price);
+    if(current_item.id != 0) {
+      y_pos = i * (MENU_ITEM_CONTAINER_HEIGHT + MARGIN) + HEADER_SIZE;
+      tft.fillRect(MARGIN, y_pos, tft.width() - MARGIN * 2, MENU_ITEM_CONTAINER_HEIGHT, BLACK);
+      tft.setCursor(MARGIN * 2, y_pos + 10);
+      tft.print(current_item.title);
+      tft.setCursor(tft.width() - MARGIN - 40, y_pos + 10);
+      tft.print("$");
+      tft.print(current_item.price);
+    }
   }
 
   tft.setTextColor(BLACK);
@@ -116,6 +128,20 @@ void draw_page() {
   tft.setCursor(tft.width() - MARGIN - 40, tft.height() - MARGIN - 15);
   tft.print("$");
   tft.print(total_cost);
+}
+
+void confirm_order() {
+  tft.fillScreen(GREEN);
+  tft.setTextSize(3);
+  tft.setTextColor(WHITE);
+  tft.setCursor(tft.width() / 2 - 45, tft.height() / 2 - 11);
+  tft.print("Order");
+  tft.setCursor(tft.width() / 2 - 85, tft.height() / 2 + 11);
+  tft.print("confirmed!");
+  delay(3000);
+  total_cart_items = 0;
+  total_cost = 0;
+  draw_page();
 }
 
 void setup(void) {
@@ -138,42 +164,76 @@ void setup(void) {
   Serial.println("connecting...");
 
   if (client.connect(server, 80)) {
+    connected_to_server = 1;
     Serial.println("connected");
     // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
+    client.println("GET /kitchen/menu/ HTTP/1.1");
+    client.println("Host: 104.131.189.231");
     client.println("Connection: close");
     client.println();
   } else {
     // if you didn't get a connection to the server:
     Serial.println("connection failed");
   }
-  
-  while (client.available()) {
-    char c = client.read();
-  }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    client.stop();
-  }
-  
-  int i = 0;
-  MenuItem item;
-
-  char title[10] = "Potato";
-
-  for(i = 0; i < 20; i++) {
-    item.id = i;
-    item.title[0] = 0x0;
-    memcpy(&item.title, (void *)title, String("Potato").length() + 1);
-    item.price = 1 + i;
-    memcpy(&menu_list[i], &item, sizeof(MenuItem));
-  }
-
-  draw_page();
  
   pinMode(13, OUTPUT);
+}
+
+void get_server_content()
+{
+  char c = 0, in_obj = 0, in_body = 0;
+  String json_raw = "";
+  MenuItem new_item;
+  const char * title_ptr = 0x0;
+  
+  while(client.available()) {
+    c = client.read();
+
+    if(in_body) {
+      if((!in_obj && c == ',') || c == ']') {
+        Serial.println(json_raw);
+        
+        StaticJsonBuffer<200> jsonBuffer;
+        JsonObject& root = jsonBuffer.parseObject(json_raw.c_str());
+      
+        if(!root.success()) {
+          Serial.println("JSONparseFailed");
+        } else {
+          title_ptr = root["title"];
+          memcpy(&new_item.title, title_ptr, strlen(title_ptr) + 1);
+          new_item.price = root["price"];
+          new_item.id = root["id"];
+          memcpy(&menu_list[num_items], &new_item, sizeof(MenuItem));
+          num_items++;
+        }
+        
+        json_raw.remove(0); // Reset string.
+      } else if(c != '[') {
+        if(c == '{') in_obj = 1;
+        else if(c == '}') in_obj = 0;
+        
+        json_raw.concat(c);
+      }
+    } else if(c == '\n' || c == '\r') {
+      json_raw.concat(c);
+
+      if(json_raw == "\r\n\r\n") {
+        in_body = 1;
+        json_raw.remove(0);
+      }
+    } else {
+      json_raw.remove(0);
+    }
+    
+    if (!client.connected()) {
+      Serial.println("Disconnected!");
+      client.stop();
+      connected_to_server = 0;
+    }
+  }
+
+  Serial.println("Drawing page.");
+  draw_page();
 }
 
 #define MINPRESSURE 10
@@ -191,6 +251,10 @@ void loop()
   pinMode(YP, OUTPUT);
   //pinMode(YM, OUTPUT);
 
+  if(connected_to_server) {
+    get_server_content();
+  }
+
   if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
     /*
     Serial.print("X = "); Serial.print(p.x);
@@ -202,12 +266,15 @@ void loop()
     p.x = map(p.x, TS_MINX, TS_MAXX, tft.width(), 0);
     p.y = (tft.height()-map(p.y, TS_MINY, TS_MAXY, tft.height(), 0));
 
-    currentcolor = 0xEEEE;
-    
-    if ((p.y-PENRADIUS) && ((p.y+PENRADIUS) < tft.height())) {
-      tft.fillCircle(p.x, p.y, PENRADIUS, currentcolor);
+    // Confirm button pressed, post order to server and reset order
+    if (p.x >= CONFIRM_BOX_X && p.x <= (NEXT_BOX_X - MARGIN) && p.y >= CONFIRM_BOX_Y && p.y <= (CONFIRM_BOX_Y + BOXSIZE)) {
+      // Post order to server somehow
+      if (total_cart_items > 0) { // and if order sent:
+        confirm_order();
+      }
     }
-    
+
+    // Menu item pressed, add to cart
     if(p.y >= HEADER_SIZE && p.x >= MARGIN && p.y <= (tft.height() - MARGIN))
     {
       int box_index_pressed = (p.y - HEADER_SIZE) / (MENU_ITEM_CONTAINER_HEIGHT + MARGIN);
@@ -233,7 +300,7 @@ void loop()
       draw_page();
     } else if ((p.x >= NEXT_BOX_X && p.x <= (NEXT_BOX_X + BOXSIZE)) && (p.y >= NEXT_BOX_Y && p.y <= (NEXT_BOX_Y + BOXSIZE))) {
       // "Next Page" button pressed.
-      if(current_page < 5) {
+      if((num_items % ITEMS_PER_PAGE == 0) ? (current_page < (num_items / ITEMS_PER_PAGE)) : (current_page < (num_items / ITEMS_PER_PAGE) + 1)) {
         current_page++;
       }
 
